3b5f6dc4a1e0e0c786af7e00f9ac217b67b45c8f
AlenaDiffy <alenadernova@gmail.com>
2022-02-06 01:01:07 +0300

    added new test with negative income

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,4 +16,26 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIfAllIncomeAreEquals() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIfOneOfIncomeIsNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
